412657145f90736c29f78d0be27c515fb1b1f254
Jody Hoon-Starr <jodyhoonstarr@gmail.com>
2022-11-01 16:12:33 -0400

    fix: set-output deprecated

--- entrypoint.sh
+++ entrypoint.sh
@@ -61,7 +61,7 @@ then
     echo -e "$RED Failed $RESULT: $NC"
     grep -E 'BROKEN' <<< "$OUTPUT" | awk '{print "[✗] " $2 "\n" }'
     echo -e "$PURPLE ============================== $NC"
-    echo ::set-output name=result::"$RESULT"
+    echo "result=$RESULT" >> $GITHUB_OUTPUT
     exit 1
 elif [ "$TOTAL_COUNT" == 0 ]
 then
@@ -69,7 +69,7 @@ then
 else 
     RESULT="✓ Checked $TOTAL_COUNT link(s), no broken links found!"
     echo -e "$GREEN $RESULT $NC"
-    echo ::set-output name=result::"$RESULT"
+    echo "result=$RESULT" >> $GITHUB_OUTPUT
     echo -e "$PURPLE ============================== $NC"
 fi
 exit 0
